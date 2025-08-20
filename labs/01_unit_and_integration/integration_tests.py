08eeb8c5884c3b056f50e445bc415c55e2fa7dcb
Arshan-git <arshan.nice@gmail.com>
2025-08-20 14:23:23 +0200

    lab1

--- labs/01_unit_and_integration/integration_tests.py
+++ labs/01_unit_and_integration/integration_tests.py
@@ -21,3 +21,17 @@ def test_addition():
 ## Complete the following tests
 
 # def test_sum_one_positive_one_negative():
+
+
+def test_addition():
+    # Arrange
+    possitive_value = 2
+    negative_value  = -3
+    url_params = {"a": possitive_value, "b": negative_value}
+    expected_result = {"result": -1}
+
+    # Act
+    response = requests.get(f"{BASE_URL}/addition", params=url_params)
+
+    # Assert
+    assert response.json() == expected_result
--- labs/01_unit_and_integration/unit_tests.py
+++ labs/01_unit_and_integration/unit_tests.py
@@ -1,4 +1,5 @@
 import main
+import pytest
 
 def test_sum_two_positives():
     # Arrange
@@ -38,3 +39,43 @@ def test_sum_two_positives():
 
 
 # def test_divide_by_zero():
+
+def test_sum_one_positive_one_negative():
+    # Arrange
+    possitive_value_1 = 1
+    negative_value_2 = -1
+    expected_result = {"result": 0 }
+
+    # Act
+    result = main.addition(possitive_value_1,negative_value_2)
+
+    # Assert
+    assert  result == expected_result
+
+    
+def test_sum_one_positive_one_string_value():
+        
+    possitive_value_1 = 1
+    string_value_2 = 'abc'
+
+    with pytest.raises(TypeError):
+        main.addition(possitive_value_1,string_value_2)
+
+    
+def test_divide_two_positive_values():
+    
+    positive_value_1 = 10 
+    positive_value_2 = 2
+    expected_result = {'result': 5}
+
+    result = main.division(positive_value_1,positive_value_2)
+    assert result == expected_result
+
+    
+def test_divide_by_zero():
+    postive_value_1 = 10
+    zero_value_2 = 0
+    
+    with pytest.raises(ZeroDivisionError):
+        main.division(postive_value_1, zero_value_2)
+    
