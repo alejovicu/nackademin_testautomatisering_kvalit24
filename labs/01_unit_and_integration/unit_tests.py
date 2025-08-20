de4a11317a18a4efa937c5fa7f08c97e9113f5d8
Adrian <adrian.valtersson@hotmail.com>
2025-08-20 14:39:32 +0200

    hopefully this is complete,adrian.valtersson@hotmail.com

--- labs/01_unit_and_integration/unit_tests.py
+++ labs/01_unit_and_integration/unit_tests.py
@@ -1,4 +1,5 @@
 import main
+import pytest
 
 def test_sum_two_positives():
     # Arrange
@@ -17,24 +18,38 @@ def test_sum_two_positives():
 
 ## Complete the following tests
 
-# def test_sum_one_positive_one_negative():
-#     # Arrange
-#     possitive_value = **
-#     negative_value  = **
-#     expected_result = {"result": ** }
-
-#     # Act
-#     result = main.sum( ** , ** )
-
-#     # Assert
-#     assert  result == expected_result
+def test_sum_one_positive_one_negative():
+    positive_value = 10
+    negative_value  = -5
+    expected_result = {"result": 5 }
+    result = main.addition(positive_value, negative_value)
+    assert  result == expected_result
 
 
 
-# def test_sum_one_positive_one_string_value():
+def test_sum_one_positive_one_string_value():
+    #Arrange
+    positive_value = 10
+    string_value = '5'
+    #Act
+    with pytest.raises(TypeError) as excinfo:
+        main.addition(positive_value, string_value)
+        assert "unsupported operand type" in str(excinfo.value)
+    
+    
+def test_divide_two_positive_values():
+    positive_value1 = 25
+    positive_value2 = 5
+    expected_result = {"result": 5}
+    
+    result = main.division(positive_value1,positive_value2)
+    assert result == expected_result
 
 
-# def test_divide_two_positive_values():
 
-
-# def test_divide_by_zero():
+def test_divide_by_zero():
+    positive_value1 = 25
+    positive_value2 = 0    
+    with pytest.raises(ZeroDivisionError):
+        main.division(positive_value1,positive_value2)
+    
