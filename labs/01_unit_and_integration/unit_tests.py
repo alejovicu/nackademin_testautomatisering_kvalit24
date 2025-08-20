6e0151ec4996b430ed610b9bc8f578f47318bf46
MaleneRiise <mlne@live.se>
2025-08-20 14:40:41 +0200

    Gjort labb hoppas allt ok, malene.riisedonev@yh.nackademin.se

--- labs/01_unit_and_integration/unit_tests.py
+++ labs/01_unit_and_integration/unit_tests.py
@@ -17,24 +17,56 @@ def test_sum_two_positives():
 
 ## Complete the following tests
 
-# def test_sum_one_positive_one_negative():
-#     # Arrange
-#     possitive_value = **
-#     negative_value  = **
-#     expected_result = {"result": ** }
+def test_sum_one_positive_one_negative():
+    # Arrange
+    possitive_value = 10
+    negative_value  = -5
+    expected_result = {"result": 5 }
 
-#     # Act
-#     result = main.sum( ** , ** )
+    # Act
+    result = main.addition(possitive_value, negative_value )
 
-#     # Assert
-#     assert  result == expected_result
-
-
-
-# def test_sum_one_positive_one_string_value():
+    # Assert
+    assert  result == expected_result
 
 
-# def test_divide_two_positive_values():
+
+def test_sum_one_positive_one_string_value():
+    # Arrange
+    positive_value = 10
+    string_value = "hej"
+
+    # Act 
+    try:
+        main.addition(positive_value, string_value)
+    # Assert
+        assert False, "Expected TypeError but no error was raised"
+    except TypeError:
+        assert True
 
 
-# def test_divide_by_zero():
+def test_divide_two_positive_values():
+    # Arrange
+    possitive_value1 = 10
+    possitive_value2  = 2
+    expected_result = {"result": 5 }
+
+    # Act
+    result = main.division(possitive_value1, possitive_value2 )
+
+    # Assert
+    assert  result == expected_result
+
+
+def test_divide_by_zero():
+    # Arrange
+    positive_value1 = 10
+    positive_value2 = 0
+
+    # Act 
+    try:
+        main.division(positive_value1, positive_value2)
+    # Assert
+        assert False, "Expected ZeroDivisionError but no error was raised"
+    except ZeroDivisionError:
+        assert True
