ab0d98261e8a9788fc1fce2c31760e3dc4970578
Caroline Enmyren <caroline.enmyren@gmail.com>
2025-08-20 14:02:30 +0200

    labb 01 first commit

--- labs/01_unit_and_integration/integration_tests.py
+++ labs/01_unit_and_integration/integration_tests.py
@@ -4,9 +4,9 @@ BASE_URL = "http://127.0.0.1:8000"
 
 def test_addition():
     # Arrange
-    possitive_value = 2
-    negative_value  = 3
-    url_params = {"a": possitive_value, "b": negative_value}
+    positive_value_1 = 2
+    positive_value_2  = 3
+    url_params = {"a": positive_value_1, "b": positive_value_2}
     expected_result = {"result": 5}
 
     # Act
@@ -14,10 +14,26 @@ def test_addition():
 
     # Assert
     assert response.json() == expected_result
+    assert response.status_code == 200
 
 
 # Lab tasks
 
 ## Complete the following tests
 
-# def test_sum_one_positive_one_negative():
+def test_sum_one_positive_one_negative():
+
+    # Arrange
+    positive_value = 2
+    negative_value  = -3
+    url_params = {"a": positive_value, "b": negative_value}
+    expected_result = {"result": -1}
+
+    # Act
+    response = requests.get(f"{BASE_URL}/addition", params=url_params)
+
+    # Assert
+    assert response.json() == expected_result  
+    assert response.status_code == 200  
+
+
