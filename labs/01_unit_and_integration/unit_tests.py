a28269e17e83632fd24e7470ea5c037d9342d91b
Jacob Å <jacob.akerblom.gc@gmail.com>
2025-08-20 13:12:00 +0200

    first commit - create branch

--- labs/01_unit_and_integration/unit_tests.py
+++ labs/01_unit_and_integration/unit_tests.py
@@ -1,37 +1,48 @@
 import main
 
+
 def test_sum_two_positives():
     # Arrange
     possitive_value_1 = 1
     possitive_value_2 = 4
-    expected_result = {"result": 5 }
+    expected_result = {"result": 5}
 
     # Act
-    result = main.addition(possitive_value_1,possitive_value_2)
+    result = main.addition(possitive_value_1, possitive_value_2)
 
     # Assert
-    assert  result == expected_result
+    assert result == expected_result
 
 
 # Lab tasks
 
 ## Complete the following tests
 
-# def test_sum_one_positive_one_negative():
-#     # Arrange
-#     possitive_value = **
-#     negative_value  = **
-#     expected_result = {"result": ** }
 
-#     # Act
-#     result = main.sum( ** , ** )
+def test_sum_one_positive_one_negative():
+    # Arrange
+    possitive_value = 5
+    negative_value = -2
+    expected_result = {"result": 3}
 
-#     # Assert
-#     assert  result == expected_result
+    # Act
+    result = main.addition(possitive_value, negative_value)
+
+    # Assert
+    assert result == expected_result
 
 
+def test_sum_one_positive_one_string_value():
+    # Arrange
+    possitive_value = 5
+    string_value = "3"
+    expected_result = {"result": TypeError}
 
-# def test_sum_one_positive_one_string_value():
+    # Act
+    result = main.addition(possitive_value, string_value)
+
+    # Assert
+    assert result == expected_result
 
 
 # def test_divide_two_positive_values():
