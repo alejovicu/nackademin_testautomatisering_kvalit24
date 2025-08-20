258c6d7292a799c9e8f2d477233a4c531dd39255
Jesper Sjöberg <jesper26sjoberg@live.se>
2025-08-20 13:36:53 +0200

    Lab 1 complete

--- labs/01_unit_and_integration/integration_tests.py
+++ labs/01_unit_and_integration/integration_tests.py
@@ -2,11 +2,12 @@ import requests
 
 BASE_URL = "http://127.0.0.1:8000"
 
+
 def test_addition():
     # Arrange
-    possitive_value = 2
-    negative_value  = 3
-    url_params = {"a": possitive_value, "b": negative_value}
+    positive_value = 2
+    negative_value = 3
+    url_params = {"a": positive_value, "b": negative_value}
     expected_result = {"result": 5}
 
     # Act
@@ -20,4 +21,15 @@ def test_addition():
 
 ## Complete the following tests
 
-# def test_sum_one_positive_one_negative():
+
+def test_sum_one_positive_one_negative():
+    positive_value = 2
+    negative_value = -3
+    url_params = {"a": positive_value, "b": negative_value}
+    expected_result = {"result": -1}
+
+    # Act
+    response = requests.get(f"{BASE_URL}/addition", params=url_params)
+
+    # Assert
+    assert response.json() == expected_result
