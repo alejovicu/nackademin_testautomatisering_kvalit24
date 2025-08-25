6c7fde09ce2bddf8b5a0fc314600d75bf16a3f8b
Saga Andersson <sagander@hotmail.com>
2025-08-25 10:27:53 +0200

    Saga Andersson saga.andersson@yh.nackademin.se: lab_01 unit & integration tests

--- labs/01_unit_and_integration/unit_tests.py
+++ labs/01_unit_and_integration/unit_tests.py
@@ -1,4 +1,5 @@
 import main
+import pytest
 
 def test_sum_two_positives():
     # Arrange
@@ -17,24 +18,49 @@ def test_sum_two_positives():
 
 ## Complete the following tests
 
-# def test_sum_one_positive_one_negative():
-#     # Arrange
-#     possitive_value = **
-#     negative_value  = **
-#     expected_result = {"result": ** }
+def test_sum_one_positive_one_negative():
+    # Arrange
+    positive_value = 5
+    negative_value  = -2
+    expected_result = {"result": 3 }
 
-#     # Act
-#     result = main.sum( ** , ** )
+    # Act
+    result = main.addition(positive_value, negative_value)
 
-#     # Assert
-#     assert  result == expected_result
-
-
-
-# def test_sum_one_positive_one_string_value():
+    # Assert
+    assert  result == expected_result
 
 
-# def test_divide_two_positive_values():
+
+def test_sum_one_positive_one_string_value():
+    # Arrange
+    positive_value = 5
+    string_value = "5"
+
+    # Act / Assert
+    # Expects type error
+    with pytest.raises(TypeError):
+        main.addition(positive_value, string_value)
+
+def test_divide_two_positive_values():
+    # Arrange
+    positive_value_1 = 10
+    positive_value_2 = 2
+    expected_result = {"result": 5}
+
+    # Act
+    result = main.division(positive_value_1, positive_value_2)
+
+    # Assert
+    assert result == expected_result
 
 
-# def test_divide_by_zero():
+def test_divide_by_zero():
+    # Arrange
+    value_1 = 10
+    value_2 = 0
+
+    # Act / Assert
+    # Expects ZeroDivisionError
+    with pytest.raises(ZeroDivisionError):
+        main.division(value_1, value_2)
