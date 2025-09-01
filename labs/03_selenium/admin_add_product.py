0b840c695e205f89a9ce09f932df9ca154eb2e56
Mahta Ghorbani <mahta_gh@hotmail.com>
2025-09-02 00:45:51 +0200

    Added signup and admin_add_product selenium tests

--- labs/03_selenium/admin_add_product.py
+++ labs/03_selenium/admin_add_product.py
@@ -1,49 +1,44 @@
-# Given I am a new potential customer​
-# When I signup in the app​
-# Then I should be able to log in with my new user
+# Given I am an admin user
+# When I add a product to the catalog
+# Then The product is available to be used in the app
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
 APP_URL='http://localhost:5173'
-
 options = Options()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-
-def test_create_new_user():
+def test_add_product():
 
     #Arrange
-    username = "admin_test"
-    password = "admin123"
     driver = webdriver.Chrome(options=options)
     driver.get(APP_URL)
-
-    signup_btn = driver.find_element("id", "login")
-    signup_btn.click()
+    driver.delete_all_cookies()
     time.sleep(1)
+
 
     input_username = driver.find_element("xpath", '//input[@placeholder="Username"]')
     input_password = driver.find_element("xpath", '//input[@placeholder="Password"]')
 
-    input_username.send_keys(username)
-    input_password.send_keys(password)
+    input_username.send_keys("admin_test")
+    input_password.send_keys("admin123")
 
     submit_btn = driver.find_element("class name", "button-primary")
     submit_btn.click()
-    time.sleep(2)
+    time.sleep(5)
 
+    input_name = driver.find_element("xpath", '//input[@placeholder="name"]')
+    input_name.send_keys("Test Product")
 
-    #Act
-    signup_submit_btn = driver.find_element("class name", "button-primary")
-    signup_submit_btn.click()
-    print(f"User {username} created")
+    add_product_btn = driver.find_element("id", "add-product")
+    add_product_btn.click()
+    print("Product added")
 
     time.sleep(5)
 
-    #Tear down
+    # Tear down
     driver.quit()
 
-
 if __name__ == "__main__":
-    test_create_new_user()
+    test_add_product()
