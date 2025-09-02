e320fd7ab9050bc99a5219a8f4ccc12c24117877
arinagran <arinagran@gmail.com>
2025-09-02 17:19:15 +0200

    lab_05_Arina_Granoboy

--- labs/05_pom/tests/test_product.py
+++ labs/05_pom/tests/test_product.py
@@ -1,25 +1,40 @@
-from playwright.sync_api import Page, expect
-
+import time
+from playwright.sync_api import Page
 from models.home import HomePage
 from models.login import LoginPage
 
 
 def test_add_product_to_catalog(page: Page):
-
-    #PO usage example
     home_page = HomePage(page)
     login_page = LoginPage(page)
+
+    # Login as admin
     home_page.navigate()
-    login_page.navigate_to_signup()
+    login_page.login_as_admin()
 
+    # Add a product to the catalog
+    product_name = f"IT course {int(time.time() * 1000)}"
+    home_page.add_product(product_name)
 
-    # Given I am an admin user​
-    # When I add a product to the catalog​
-    # Then The product is available to be used in the app
-    pass
+    # Product is available in the app
+    home_page.assert_product_visible(product_name)
+
 
 def test_remove_product_from_catalog(page: Page):
-    # Given I am an admin user​
-    # When I remove a product from the catalog​
-    # Then The product should not be listed in the app to be used
-    pass
+    home_page = HomePage(page)
+    login_page = LoginPage(page)
+
+    # Login as admin
+    home_page.navigate()
+    login_page.login_as_admin()
+
+    # Add a product to the catalog
+    product_name = f"IT course {int(time.time() * 1000)}"
+    home_page.add_product(product_name)
+    home_page.assert_product_visible(product_name)
+
+    # Remove product from the catalog
+    home_page.remove_product(product_name)
+
+    # Product is not visible in the app
+    home_page.assert_product_not_visible(product_name)
