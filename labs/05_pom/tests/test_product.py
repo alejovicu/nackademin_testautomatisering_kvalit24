70853f32e3c7ac12a0549d7729544cf2c33a0ab7
Linn Enmyren <caroline.enmyren@yh.nackademin.se>
2025-09-02 10:39:56 +0200

    Linn Enmyren, caroline.enmyren@yh.nackademin.se

--- labs/05_pom/tests/test_product.py
+++ labs/05_pom/tests/test_product.py
@@ -2,24 +2,45 @@ from playwright.sync_api import Page, expect
 
 from models.home import HomePage
 from models.login import LoginPage
+from models.signup import SignUpPage
+from models.product_page import ProductPage
+import time
 
 
-def test_add_product_to_catalog(page: Page):
+#SET UP
+BASE_URL = "http://localhost:5173/"
+username = "admin"
+password = "admin123"
 
-    #PO usage example
-    home_page = HomePage(page)
+
+def test_add_and_remove_product_to_catalog(page: Page):
+
+    # Skapar objekt av klasserna ("Models")
+    home_page = HomePage(page) 
     login_page = LoginPage(page)
-    home_page.navigate()
-    login_page.navigate_to_signup()
+    signup_page = SignUpPage(page)
+    product_page = ProductPage(page)
 
+    # Genererar produktnamn
+    new_product = f"product_{int(time.time())}"
 
-    # Given I am an admin user​
-    # When I add a product to the catalog​
-    # Then The product is available to be used in the app
-    pass
+    # Go to homepage
+    home_page.navigate() 
 
-def test_remove_product_from_catalog(page: Page):
-    # Given I am an admin user​
-    # When I remove a product from the catalog​
-    # Then The product should not be listed in the app to be used
-    pass
+    # Attempt sign-up
+    login_page.navigate_to_signup() 
+    signup_page.signup(username, password)
+
+    # Login + assert admin
+    signup_page.navigate_to_login()
+    login_page.login(username, password)
+    expect(page.get_by_role("button", name="Create Product")).to_be_visible()
+
+    # Add product + assert
+    count_before_adding = product_page.add_product(new_product)
+    expect(product_page.product.nth(-1)).to_contain_text(new_product)
+    expect(product_page.product).to_have_count(count_before_adding + 1)
+
+    # Delete product + assert
+    deleted_product = product_page.delete_product(new_product)
+    expect(deleted_product).to_have_count(0)
