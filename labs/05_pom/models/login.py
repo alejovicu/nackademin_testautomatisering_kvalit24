a5189810d753c5de3289c16857f4933d3f3e624d
Jacob Åkerblom - jacob.akerblom@yh.nackademin.se <jacob.akerblom.gc@gmail.com>
2025-09-03 21:59:01 +0200

    Lab 05 jacob åkerblom - jacob.akerblom@yh.nackademin.se

--- labs/05_pom/models/login.py
+++ labs/05_pom/models/login.py
@@ -2,13 +2,20 @@
 # 2 inputs and 1 button
 # Naming example:  input_username
 
+
 class LoginPage:
     def __init__(self, page):
         self.page = page
-        #self.input_username = page.locator(??)
-        #self.input_password = page.locator(??)
-        #self.button_login = page.locator(??)
+        self.header_main_title = page.get_by_text("Nackademin Course App")
+        self.input_username = page.get_by_placeholder("Username")
+        self.input_password = page.get_by_placeholder("Password")
+        self.button_login = page.locator("button.button-primary")
         self.button_signup = page.locator("#signup")
 
     def navigate_to_signup(self):
         self.button_signup.click()
+
+    def login_user(self, username, password):
+        self.input_username.fill(username)
+        self.input_password.fill(password)
+        self.button_login.click()
