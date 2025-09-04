aeadc4e71b105b00bff9578131acb05377b43483
MimmiGibbs <mimmisethfors@gmail.com>
2025-09-04 10:54:09 +0200

    lab_05_mimmi_gibbs

--- labs/05_pom/models/login.py
+++ labs/05_pom/models/login.py
@@ -1,14 +1,13 @@
-# Implement PO for login
-# 2 inputs and 1 button
-# Naming example:  input_username
-
 class LoginPage:
     def __init__(self, page):
         self.page = page
-        #self.input_username = page.locator(??)
-        #self.input_password = page.locator(??)
-        #self.button_login = page.locator(??)
-        self.button_signup = page.locator("#signup")
+        self.login_header_main_title = page.get_by_text('Nackademin Course App') 
+        self.login_input_username = page.get_by_placeholder('Username')
+        self.login_input_password = page.get_by_placeholder('Password')
+        self.login_button = page.get_by_role("button", name="Login")
 
-    def navigate_to_signup(self):
-        self.button_signup.click()
+   
+    def login(self,username,password):
+        self.login_input_username.fill(username)
+        self.login_input_password.fill(password)
+        self.login_button.click()
