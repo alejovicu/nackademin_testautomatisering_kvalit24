91eff1beda35f14594fa28f91391a39f43d000f1
nermin mahsan <nerminmahsan@MacBook-Air-som-tillhor-nermin.local>
2025-09-07 14:46:26 +0200

    Labb05 Nermin ( narmin.saleh@yh.nackademin.se)

--- labs/05_pom/tests/test_product.py
+++ labs/05_pom/tests/test_product.py
@@ -1,25 +1,41 @@
 from playwright.sync_api import Page, expect
 
 from models.home import HomePage
-from models.login import LoginPage
+from models.admin_login import LoginPage
+from models.admin_page import AdminPage
+
 
 
 def test_add_product_to_catalog(page: Page):
 
-    #PO usage example
-    home_page = HomePage(page)
+    #Given I am an admin user
     login_page = LoginPage(page)
-    home_page.navigate()
-    login_page.navigate_to_signup()
+    login_page.navigate_to_login()
+    login_page.admin_login("nermin123", "nermin_123")
 
 
-    # Given I am an admin user​
     # When I add a product to the catalog​
-    # Then The product is available to be used in the app
-    pass
+    product_page = AdminPage(page)
+    product_page.navigate_to_create_product()
+    product_page.add_product("Lemon")
 
-def test_remove_product_from_catalog(page: Page):
+    # Then The product is available to be used in the app
+    expect(page.get_by_text("New product")).to_be_visible()
+    
+
+def test_remove_product_from_catalog(page: Page, product = "Lemon"):
+
     # Given I am an admin user​
+    login_page = LoginPage(page)
+    login_page.navigate_to_login()
+    login_page.admin_login("nermin123", "nermin_123")
+
     # When I remove a product from the catalog​
+    admin_page = AdminPage(page)
+    before_count = admin_page.count_products()
+    admin_page.delete_product(product)
+   
+
     # Then The product should not be listed in the app to be used
-    pass
+    expect(admin_page.products).to_have_count(before_count - 1)
+    expect(page.get_by_text(product)).not_to_be_visible()
