81663d91adc007e9421de89cd6c34b3d2394a898
Safia Usman <safia.usman@outlook.com>
2025-09-07 22:22:59 +0200

    done

--- labs/04_play_with_playwright/test_product.py
+++ labs/04_play_with_playwright/test_product.py
@@ -1,10 +1,27 @@
 import re
-from playwright.sync_api import Page, expect
+import time
+import pytest
+from playwright.sync_api import Page, expect, sync_playwright
 
 def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
     expect(page).to_have_title(re.compile("Playwright"))
     expect(page.get_by_text("Get started")).to_be_visible()
+
+'''@pytest.fixture(scope="session")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=100)  # 👈 See browser
+        yield browser
+        browser.close()
+
+@pytest.fixture
+def page(browser):
+    context = browser.new_context()
+    page = context.new_page()
+    yield page
+    time.sleep(30000)
+    context.close()'''
 
 
 #Given I am an admin user​
@@ -23,23 +40,31 @@ def login_as_admin(page: Page):
     expect(page.get_by_text("products available")).to_be_visible()
 
 
-def test_add_product(page: Page,product="blue berries"):
+def test_add_product(page: Page,product="choclate"):
     login_as_admin(page)
     page.get_by_placeholder("Product name").fill(product)
     page.get_by_role("button", name="Create Product").click()
 
+    added_product=page.locator(".product-item", has_text= product)
     expect(page.get_by_text("products available")).to_be_visible()
-    #expect(page.locator(".product-item", has_text= product)).first_to_be_visible()
-    #expect(page.locator(".product-item", has_text= product)).to_have_count(1)
+    expect(added_product.nth(0)).to_be_visible()
+   
+  
 
 
-def test_delete_product(page: Page, product="Coca cola"):
+def test_delete_product(page: Page, product="apples"):
     login_as_admin(page)
-   #<button class="product-item-button">Delete</button>
-    product_item=page.locator(".product-item", has_text = product)
-    product_item.get_by_role("button", name="Delete").click()
+ 
+    product_items=page.locator(".product-item", has_text = product)
+    product_items.nth(0).get_by_role("button", name="Delete").click()   #first matching product
+
+    initial_count=product_items.count()
 
     expect(page.get_by_text("products available")).to_be_visible()
-    expect(page.locator(".product-item",has_text=product)).to_have_count(0)
+    expect (product_items).to_have_count(initial_count-1)
+    #expect(page.locator(".product-item", has_text= product)).not_to_be_visible()
+
+ 
+   
 
 
