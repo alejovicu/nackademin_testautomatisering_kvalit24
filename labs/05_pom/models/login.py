f00640a977a5161868d18b6638dfe7a6e8d2c9fa
Safia Usman <safia.usman@outlook.com>
2025-09-08 10:55:10 +0200

    pom update

--- labs/05_pom/models/login.py
+++ labs/05_pom/models/login.py
@@ -5,10 +5,24 @@
 class LoginPage:
     def __init__(self, page):
         self.page = page
-        #self.input_username = page.locator(??)
-        #self.input_password = page.locator(??)
-        #self.button_login = page.locator(??)
+        self.input_username = page.get_by_placeholder("Username")
+        self.input_password = page.get_by_placeholder("Password")
+        self.button_login = page.locator("button.button-primary")  # CSS 
         self.button_signup = page.locator("#signup")
+
+
+    def navigate(self):
+        self.page.goto("https://localhost:5173")
+
 
     def navigate_to_signup(self):
         self.button_signup.click()
+
+
+    def login_as_admin(self):
+        self.input_username.fill("admin")
+        self.input_password.fill("admin")
+        self.button_login.click()
+        
+
+
