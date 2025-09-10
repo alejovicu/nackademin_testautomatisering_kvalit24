49572aff827b2b5f58c154d4ed504395f6736f1f
Sara Känngård <sara.kanngard@gmail.com>
2025-09-10 12:35:38 +0200

    integrations tests done?

--- labs/06_07_08_test_framework/test/integration/test_products.py
+++ labs/06_07_08_test_framework/test/integration/test_products.py
@@ -1,34 +1,38 @@
+from playwright.sync_api import Page
 import libs.utils
 import pytest
 from models.api.user import UserAPI
 from models.api.admin import AdminAPI
+# from dotenv import load_dotenv
 
 
-# Given I am an admin user​
-# When I add a product to the catalog​
-# Then The product is available to be used in the app
 def test_add_product_to_catalog():  # complete code
     user_api = UserAPI("http://localhost:8000")
     admin_api = AdminAPI("http://localhost:8000")
     username = "Admin_user"
     password = "Automation53"
+    # Given I am an admin user​
     admin_token = user_api.login_token(username, password)
     admin_api.set_token(admin_token)
-    admin_api.create_product("New product")
+    # When I add a product to the catalog​
+    new_product_added = admin_api.create_product("New product")
+    new_product_name = admin_api.get_product_by_name("New product")
+    # Then The product is available to be used in the app
+    assert new_product_name == "New product"
+    assert new_product_added.status_code == 200
 
-    pass
 
-
-# Given I am an admin user​
-# When I remove a product from the catalog​
-# Then The product should not be listed in the app to be used
 def test_remove_product_from_catalog():  # complete code
     user_api = UserAPI("http://localhost:8000")
     admin_api = AdminAPI("http://localhost:8000")
     username = "Admin_user"
     password = "Automation53"
+    # Given I am an admin user​
     admin_token = user_api.login_token(username, password)
     admin_api.set_token(admin_token)
-    admin_api.delete_product_by_name("New product")
-
-    pass
+    # When I remove a product from the catalog​
+    delete_product = admin_api.delete_product_by_name("New product")
+    # Then The product should not be listed in the app to be used
+    removed_product = admin_api.get_product_by_name("New product")
+    assert delete_product.status_code == 200
+    assert removed_product == None
