9a7ab9e4c88f89616670f9e0a12afec5c1dcc440
MimmiGibbs <mimmisethfors@gmail.com>
2025-09-11 11:57:06 +0200

    lab 06 07 08 mimmi gibbs with injecting jwt into localstorage

--- labs/06_07_08_test_framework/test/e2e/test_products.py
+++ labs/06_07_08_test_framework/test/e2e/test_products.py
@@ -1,23 +1,28 @@
 from playwright.sync_api import Page, expect
 from models.ui.home import HomePage
 from models.ui.admin import AdminPage
-# complete imports
-
-import libs.utils
-
+import requests
 
 # Given I am an admin user​
 # When I add a product to the catalog​
 # Then The product is available to be used in the app
 def test_add_product_to_catalog(page: Page):
-    login_page = HomePage(page)
     home_page = HomePage(page)
     admin_page = AdminPage(page)
     product = "mobil"
-    home_page.navigate()
 
-    # Given I am an admin user​
-    login_page.login('admin1', '1234')
+    login_url = "http://localhost:8000/login"
+    response = requests.post(login_url, json={
+        "username": "admin1",
+        "password": "1234"
+    })
+    response.raise_for_status()
+    jwt = response.json()["access_token"]
+
+    # inject jwt into localstorage
+    page.add_init_script(f"""window.localStorage.setItem('token', '{jwt}');""")
+
+    home_page.navigate()
 
     # When I add a product to the catalog​
     expect(admin_page.products.first).to_be_visible()
@@ -27,19 +32,25 @@ def test_add_product_to_catalog(page: Page):
     # Then The product is available to be used in the app
     expect(admin_page.products).to_have_count(before_new_product + 1)
 
-
 # Given I am an admin user​
 # When I remove a product from the catalog​
 # Then The product should not be listed in the app to be used
 def test_remove_product_from_catalog(page: Page):
-    login_page = HomePage(page)
     home_page = HomePage(page)
     admin_page = AdminPage(page)
     product = "mobil"
-    home_page.navigate()
+    login_url = "http://localhost:8000/login"
+    response = requests.post(login_url, json={
+        "username": "admin1",
+        "password": "1234"
+    })
+    response.raise_for_status()
+    jwt = response.json()["access_token"]
 
-    # Given I am an admin user​
-    login_page.login('admin1', '1234')
+    # inject jwt into localstorage
+    page.add_init_script(f"""window.localStorage.setItem('token', '{jwt}');""")
+
+    home_page.navigate()
 
     expect(admin_page.products.first).to_be_visible()
 
