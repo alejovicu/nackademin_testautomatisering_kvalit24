551d2d01714d5982e10c8abafa8cf4891da2e43e
Dagim Dawit <dagim50000@gmail.com>
2025-09-11 13:29:31 +0200

    done with 5

--- labs/05_pom/models/login.py
+++ labs/05_pom/models/login.py
@@ -1,14 +1,20 @@
-# Implement PO for login
-# 2 inputs and 1 button
-# Naming example:  input_username
-
 class LoginPage:
     def __init__(self, page):
         self.page = page
-        #self.input_username = page.locator(??)
-        #self.input_password = page.locator(??)
-        #self.button_login = page.locator(??)
+        # Locators
+        self.input_username = page.get_by_placeholder("Username")
+        self.input_password = page.get_by_placeholder("Password")
+        # Correct way to find a button with role + text
+        #self.button_login = page.get_by_role("button", name="Login")
+        self.button_login = page.locator("button.button-primary")
         self.button_signup = page.locator("#signup")
 
     def navigate_to_signup(self):
+        """Navigate to the signup page by clicking the signup button"""
         self.button_signup.click()
+
+    def login(self, username, password):
+        """Fill in username and password, then click login"""
+        self.input_username.fill(username)
+        self.input_password.fill(password)
+        self.button_login.click()
--- labs/05_pom/models/signup.py
+++ labs/05_pom/models/signup.py
@@ -1,3 +1,19 @@
-# Implement PO for signup
-# 2 inputs and 1 button
-# Naming example:  signup_input_username
+class SignUpPage:
+    def __init__(self, page):
+        self.page = page
+        self.input_username = page.get_by_placeholder("Username")
+        self.input_password = page.get_by_placeholder("Password")
+        # Correct locator for the Sign Up button
+        self.button_signup = page.get_by_role("button", name="Sign Up")
+        # Locator for "Log in" button to navigate back
+        self.button_login = page.locator("button.btn-blue")
+
+    def nav_to_login(self):
+        """Navigate back to the login page"""
+        self.button_login.click()
+
+    def sign_up(self, username, password):
+        """Fill in signup form and submit"""
+        self.input_username.fill(username)
+        self.input_password.fill(password)
+        self.button_signup.click()
