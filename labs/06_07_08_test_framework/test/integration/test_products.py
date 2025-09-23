85edd8cfdafbd80de64221dc0cfef9a9fae3143e
Linn Enmyren <caroline.enmyren@yh.nackademin.se>
2025-09-23 11:03:25 +0200

    name generation

--- labs/06_07_08_test_framework/test/integration/test_products.py
+++ labs/06_07_08_test_framework/test/integration/test_products.py
@@ -12,7 +12,7 @@ def test_add_product_to_catalog():
     admin_api.get_admin_token() # token is stored in the admin-api instance
 
     # WHEN I ADD A PRODUCT TO THE CATALOGUE
-    product_name = f"product_{int(time.time())}"
+    product_name = f"product_{int(time.time() * 1000)}"
     count_before = admin_api.get_current_product_count()
     create_response = admin_api.create_product(product_name)
 
