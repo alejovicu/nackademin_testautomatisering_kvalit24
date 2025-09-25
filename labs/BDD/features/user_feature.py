654d1c5929137fcfdfb696eab19dd27ae0931132
Adrian <adrian.valtersson@hotmail.com>
2025-09-25 19:37:44 +0200

    fixed some flakey tests

---
+++ labs/BDD/features/user_feature.py
@@ -0,0 +1,19 @@
+# from pytest_bdd import (
+#     given,
+#     scenario,
+#     then,
+#     when,
+# )
+
+
+
+
+
+# Feature: User
+#     Validates user creation and authentication.
+
+
+#     Scenario: Publishing the article 
+#         Given i am an authenticated user
+#         When i log into the application
+#         Then i should see all my products
