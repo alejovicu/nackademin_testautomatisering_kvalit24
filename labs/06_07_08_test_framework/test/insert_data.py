3041383e523b6daa5f749908863368ebd59369b2
MimmiGibbs <mimmisethfors@gmail.com>
2025-09-26 10:32:54 +0200

    changed url and added data

---
+++ labs/06_07_08_test_framework/test/insert_data.py
@@ -0,0 +1,29 @@
+import requests 
+import os
+
+BACKEND_URL = os.environ.get("BACKEND_URL", "http://127.0.0.1:8000")
+
+requests.post(f"{BACKEND_URL}/signup",json={
+  "username": "admin1",
+  "password": "1234"
+})
+
+requests.post(f"{BACKEND_URL}/signup",json={
+  "username": "mimmi",
+  "password": "1234"
+})
+
+# log in as admin
+login_resp = requests.post(f"{BACKEND_URL}/login", json={"username": "admin1", "password": "1234"})
+login_resp.raise_for_status()
+token = login_resp.json()["access_token"]
+
+headers = {"Authorization": f"Bearer {token}"}
+
+# create new product
+product_resp = requests.post(f"{BACKEND_URL}/products", json={"name": "Testprodukt"}, headers=headers)
+product_resp.raise_for_status()
+
+if product_resp.status_code != 200:
+    print("Product creation failed:", product_resp.text)
+    raise SystemExit(1)
--- labs/06_07_08_test_framework/test/integration/test_products.py
+++ labs/06_07_08_test_framework/test/integration/test_products.py
@@ -7,6 +7,7 @@ import os
 
 BACKEND_URL = os.environ.get("BACKEND_URL", "http://127.0.0.1:8000")
 
+
 # Given I am an admin user​
 # When I add a product to the catalog​
 # Then The product is available to be used in the app
@@ -15,27 +16,28 @@ def test_add_product_to_catalog():
     password = "1234"
     product_name = "Testprodukt"
 
-    #logg in via user login 
+    # logg in via user login
     user_api = UserAPI(BACKEND_URL)
     api_response = user_api.login(username, password)
     assert api_response.status_code == 200
-    #get the access token and check its there
+    # get the access token and check its there
     token = api_response.json().get("access_token")
     assert token is not None
-    #go to admin with access token
-    admin_api = AdminAPI("http://127.0.0.1:8000", token)
+    # go to admin with access token
+    admin_api = AdminAPI(BACKEND_URL, token)
 
-    #check amount of products before add
+    # check amount of products before add
     api_response = admin_api.get_current_product_count()
     count_before_add = len(api_response.json())
 
-    #add a product to the catalog
+    # add a product to the catalog
     api_response = admin_api.create_product(product_name)
-    #count the products after the added one
+    # count the products after the added one
     api_response = admin_api.get_current_product_count()
     count_after_add = len(api_response.json())
     assert api_response.status_code == 200
     assert count_before_add == count_after_add - 1
+
 
 # Given I am an admin user​
 # When I remove a product from the catalog​
@@ -43,27 +45,27 @@ def test_add_product_to_catalog():
 def test_remove_product_from_catalog():
     username = "admin1"
     password = "1234"
-    product_delete_id = 6
+    product_delete_id = 1
 
-    #logg in via user login 
+    # logg in via user login
     user_api = UserAPI(BACKEND_URL)
     api_response = user_api.login(username, password)
     assert api_response.status_code == 200
-    #get the access token and check its there
+    # get the access token and check its there
     token = api_response.json().get("access_token")
     assert token is not None
-    #go to admin with access token
-    admin_api = AdminAPI("http://127.0.0.1:8000", token)
+    # go to admin with access token
+    admin_api = AdminAPI(BACKEND_URL, token)
 
-    #check the length of the list before and after the product is deleted. 
+    # check the length of the list before and after the product is deleted.
     api_response = admin_api.get_current_product_count()
     count_before_delete = len(api_response.json())
 
-    #delete by id and check if the delete happens or not
+    # delete by id and check if the delete happens or not
     delete_response = admin_api.delete_product_by_id(product_delete_id)
     assert delete_response.status_code in (200, 204)
 
-    #check and compare len after delete
+    # check and compare len after delete
     api_response = admin_api.get_current_product_count()
     count_after_delete = len(api_response.json())
     assert count_before_delete == count_after_delete + 1
