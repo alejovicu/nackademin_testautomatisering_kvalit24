dae2f4b9dfc204bd137cc00e23db5e4d5bcf4ce1
MimmiGibbs <mimmisethfors@gmail.com>
2025-09-26 10:41:55 +0200

    fixing add product

--- labs/06_07_08_test_framework/test/insert_data.py
+++ labs/06_07_08_test_framework/test/insert_data.py
@@ -13,17 +13,17 @@ requests.post(f"{BACKEND_URL}/signup",json={
   "password": "1234"
 })
 
-# log in as admin
-login_resp = requests.post(f"{BACKEND_URL}/login", json={"username": "admin1", "password": "1234"})
-login_resp.raise_for_status()
-token = login_resp.json()["access_token"]
+# # log in as admin
+# login_resp = requests.post(f"{BACKEND_URL}/login", json={"username": "admin1", "password": "1234"})
+# login_resp.raise_for_status()
+# token = login_resp.json()["access_token"]
 
-headers = {"Authorization": f"Bearer {token}"}
+# headers = {"Authorization": f"Bearer {token}"}
 
-# create new product
-product_resp = requests.post(f"{BACKEND_URL}/products", json={"name": "Testprodukt"}, headers=headers)
-product_resp.raise_for_status()
+# # create new product
+# product_resp = requests.post(f"{BACKEND_URL}/products", json={"name": "Testprodukt"}, headers=headers)
+# product_resp.raise_for_status()
 
-if product_resp.status_code != 200:
-    print("Product creation failed:", product_resp.text)
-    raise SystemExit(1)
+# if product_resp.status_code != 200:
+#     print("Product creation failed:", product_resp.text)
+#     raise SystemExit(1)
