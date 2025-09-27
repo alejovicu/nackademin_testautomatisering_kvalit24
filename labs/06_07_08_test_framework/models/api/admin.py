917a2b58c8503480446f4ebe25712e49826f2299
Safia Usman <safia.usman@outlook.com>
2025-09-27 15:11:11 +0200

    admin.py

--- labs/06_07_08_test_framework/models/api/admin.py
+++ labs/06_07_08_test_framework/models/api/admin.py
@@ -3,10 +3,7 @@ import requests
 class AdminAPI:
     def __init__(self, base_url, token):
         self.base_url = base_url
-        self.token = token
-        
-
-
+        self.token = None
 
     def login(self, username, password):
         # complete logic
@@ -15,45 +12,39 @@ class AdminAPI:
         if response.status_code == 200:
             self.token = response.json().get("token")
             return response
-        
+
+    def get_auth_headers(self):
+        return {"Authorization": f"Bearer {self.token}"}
 
     def get_all_products(self):
-        headers = {"Authorization": f"Bearer {self.token}"}
-        response = requests.get(f"{self.base_url}/products", headers=headers)
+        response = requests.get(f"{self.base_url}/products", headers = self.get_auth_headers())
         if response.status_code == 200:
             return response.json()  # assuming API returns a list of products
         else:
             raise Exception("Failed to fetch products")
-            
-    
-            
 
     def get_current_product_count(self):
         # complete logic
         # return number of total products displayed
         products=self.get_all_products()
         return len(products)
-       
-
 
     def create_product(self, product_name):
         # complete logic
-        headers = {"Authorization": f"Bearer {self.token}"}
         body = {"name": product_name}
-        response = requests.post(f"{self.base_url}/products", json=body, headers=headers)
+        response = requests.post(f"{self.base_url}/products", json=body, headers=self.get_auth_headers())
         return response
-        
-
-
+                    
     def delete_product_by_name(self, product_name):
         # complete logic
-       for product in self.get_all_products():
-            if product['name'] == product_name:
-                product_id = product['id']
-                headers = {"Authorization": f"Bearer {self.token}"}
-                url = f"{self.base_url}/products/{product_id}"
-                response = requests.delete(url, headers=headers)
-                return response.status_code == 204
+        products = self.get_all_products()
+  
+        for product in products:
+            if product["name"] == product_name:
+                return requests.delete(f"{self.base_url}/product/{product['id']}", headers=self.get_auth_headers())
+        
+        return None
+       
                  
 
 
