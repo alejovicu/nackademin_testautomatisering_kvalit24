eccc2472eee805679dcb745f3a59baf5b88cd356
Safia Usman <safia.usman@outlook.com>
2025-09-27 15:09:07 +0200

    added fasade and utils

--- labs/06_07_08_test_framework/facade/users.py
+++ labs/06_07_08_test_framework/facade/users.py
@@ -15,3 +15,17 @@ class UsersFacade:
         # navigate to login
         # login as new user
         # return username and password
+        username = libs.utils.generate_string_with_prefix("user")
+        password = "Password123!"
+        home_page = HomePage(self.page)
+        home_page.navigate()
+        home_page.go_to_signup()
+        signup_page = SignupPage(self.page)
+        signup_page.signup(username, password)
+        home_page.navigate()
+        home_page.login(username, password)
+        return username, password
+        # return username and password
+        return username, password
+    
+    
