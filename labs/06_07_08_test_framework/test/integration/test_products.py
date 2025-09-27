1813ad68e0fcf7b9f975af6e3a739852d0610ac4
Safia Usman <safia.usman@outlook.com>
2025-09-27 14:54:45 +0200

    Safia integration testing

--- labs/06_07_08_test_framework/test/integration/test_products.py
+++ labs/06_07_08_test_framework/test/integration/test_products.py
@@ -1,7 +1,23 @@
 ########NOTES:need to understand####################
 import libs.utils
 import pytest
+import time
+import os
 from models.api.admin import AdminAPI
+
+BASE_URL_BACKEND =os.getenv("BASE_URL_BACKEND", "http://localhost:8000")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "admin")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "admin")
+
+def admin_api():
+    api = AdminAPI(BASE_URL_BACKEND, token=None)
+    login_response = api.login(ADMIN_USERNAME, ADMIN_PASSWORD)
+    assert login_response.status_code == 200
+
+    token = login_response.json().get("access_token")
+    assert token is not None
+    api.token = token
+    return api
 
 
 # Given I am an admin user​
@@ -9,53 +25,46 @@ from models.api.admin import AdminAPI
 # Then The product is available to be used in the app
 def test_add_product_to_catalog():
     ## Given I am an admin user​ 
-    username="admin"
-    password="admin"
+    api=admin_api()
+    product_name=libs.utils.generate_product_with_prefix("product")
+    api.create_product(product_name)
+    print(" Created product name:", product_name)
+    products=api.get_all_products()
+    names=[product["name"] for product in products]
+    print(" All product names:", names)
+    assert product_name in names
 
-    admin_api=AdminAPI("http://localhost:8000",token=None)
-    login_response=admin_api.login(username, password)
-    assert login_response.status_code == 200
-    token=login_response.json().get("access_token")
-    assert token is not None
-    admin_api=AdminAPI("http://localhost:8000",token=token) 
-    assert admin_api.token == token
-    product_name = libs.utils.generate_product_with_prefix("product")
-    initial_product_count = admin_api.get_current_product_count()
-
-    ## When I add a product to the catalog​
-    admin_api.create_product(product_name)
-
-    ## Then The product is available to be used in the app
-    assert admin_api.get_current_product_count() == initial_product_count + 1
    
+        
+        
 
 
 
 # Given I am an admin user​
 # When I remove a product from the catalog​
 # Then The product should not be listed in the app to be used
+
+    
 def test_remove_product_from_catalog():
-    # complete code
-    # Given I am an admin user
-    admin_api = AdminAPI("http://localhost:8000", token=None)
-    login_response = admin_api.login("admin", "admin")
-    assert login_response.status_code == 200
+    api=admin_api()
+    product_name=libs.utils.generate_product_with_prefix("product")
+    api.create_product(product_name)
+    print(" Created product name:", product_name)
+    #assert create_products.status_code==200
+    #api.delete_product_by_name(product_name)
 
-    token = login_response.json().get("access_token")
-    assert token is not None
-    admin_api = AdminAPI("http://localhost:8000", token=token)
+    '''delete_response=api.delete_product_by_name(product_name)
+    print(" Deleted product name:", product_name)
+    assert delete_response.status_code== 200'''
+    
+    api.delete_product_by_name(product_name)
+    products=api.get_all_products()
+    names=[product["name"] for product in products]
+    print(" All product names:", names)
+    assert product_name not in names
 
-    # Create a product to later delete
-    '''product_name = generate_product_with_prefix("testproduct")
-    admin_api.create_product(product_name)
+   
 
-    all_products = admin_api.get_all_products()
-    assert any(p["name"] == product_name for p in all_products)
 
-    # When I remove the product from the catalog
-    admin_api.delete_product_by_name(product_name)
 
-    # Then The product should not be listed
-    updated_products = admin_api.get_all_products()
-    assert all(p["name"] != product_name for p in updated_products)'''
    
