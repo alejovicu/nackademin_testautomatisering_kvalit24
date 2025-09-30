7007ddc1e02a98d9c6d3833fe1eaa06c9dc2ec33
KKons <kkonstantina@hotmail.com>
2025-09-30 10:27:55 +0200

    added  APP_FRONTEND_URL

--- labs/06_07_08_test_framework/models/ui/home.py
+++ labs/06_07_08_test_framework/models/ui/home.py
@@ -1,5 +1,6 @@
 # Landing page where the users could either login or
 # navigate to signup
+import os
 
 
 class HomePage:
@@ -14,7 +15,8 @@ class HomePage:
         self.login_btn_signup = page.locator("#signup")
 
     def navigate(self):
-        self.page.goto("http://localhost:5173/")
+        url = os.getenv("APP_FRONTEND_URL", "http://localhost:5173/")
+        self.page.goto(url)
 
     def login(self, username, password):
         self.login_input_username.fill(username)
