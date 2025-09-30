d16dd303155916bbdcff48d6432815b7056db551
MimmiGibbs <mimmisethfors@gmail.com>
2025-09-30 09:10:00 +0200

    fixed updates

--- labs/06_07_08_test_framework/models/api/admin.py
+++ labs/06_07_08_test_framework/models/api/admin.py
@@ -1,10 +1,11 @@
 import requests
 
+
 class AdminAPI:
     def __init__(self, base_url, token):
         self.base_url = base_url
         self.token = token
-        
+
     @property
     def headers(self):
         return {
@@ -12,17 +13,20 @@ class AdminAPI:
             "Content-Type": "application/json",
         }
 
-
     def create_product(self, product_name):
-        body = {"name":product_name}
-        response = requests.post(f"{self.base_url}/products", json=body, headers = self.headers)
+        body = {"name": product_name}
+        response = requests.post(
+            f"{self.base_url}/product", json=body, headers=self.headers
+        )
         return response
-    
+
     def get_current_product_count(self):
-        response = requests.get(f"{self.base_url}/products", headers = self.headers)
+        response = requests.get(f"{self.base_url}/products", headers=self.headers)
         return response
-         
-    #i chose to delete by id instead of by name
+
+    # i chose to delete by id instead of by name
     def delete_product_by_id(self, product_id):
-        response = requests.delete(f"{self.base_url}/product/{product_id}", headers = self.headers)
+        response = requests.delete(
+            f"{self.base_url}/product/{product_id}", headers=self.headers
+        )
         return response
--- labs/06_07_08_test_framework/models/api/user.py
+++ labs/06_07_08_test_framework/models/api/user.py
@@ -17,25 +17,29 @@ class UserAPI:
         }
 
     def login(self, username, password):
-        body = {"username":username, "password":password}
+        body = {"username": username, "password": password}
         response = requests.post(f"{self.base_url}/login", json=body)
         self.token = response.json().get("access_token")
         return response
 
     def signup(self, username, password):
         # complete code
-        body = {"username":username, "password":password}
+        body = {"username": username, "password": password}
         response = requests.post(f"{self.base_url}/signup", json=body)
         return response
 
-    def get_user_profile(self): 
-        response = requests.get(f"{self.base_url}/user", headers = self.headers)
+    def get_user_profile(self):
+        response = requests.get(f"{self.base_url}/user", headers=self.headers)
         return response
 
     def assign_product_to_user(self, product_id):
-        response = requests.post(f"{self.base_url}/user/products/{product_id}", headers = self.headers)
+        response = requests.post(
+            f"{self.base_url}/user/product/{product_id}", headers=self.headers
+        )
         return response
 
     def remove_product_from_user(self, product_id):
-        response = requests.delete(f"{self.base_url}/user/product/{product_id}", headers = self.headers)
+        response = requests.delete(
+            f"{self.base_url}/user/product/{product_id}", headers=self.headers
+        )
         return response
