4192653776733f0302617d06edce909ae843f0dd
KKons <kkonstantina@hotmail.com>
2025-10-02 10:47:11 +0200

    added code from github

--- labs/06_07_08_test_framework/test/integration/test_users.py
+++ labs/06_07_08_test_framework/test/integration/test_users.py
@@ -1,7 +1,12 @@
-from playwright.sync_api import Page
 # complete imports
+import os
 import libs.utils
 from models.api.user import UserAPI
+
+
+APP_BACKEND_URL = os.getenv("APP_BACKEND_URL", "http://localhost:8000")
+USER_USERNAME = os.getenv("USER_USERNAME", "user_1")
+USER_PASSWORD = os.getenv("USER_PASSWORD", "pass1234")
 
 
 # Given I am a new potential customer​
@@ -9,17 +14,17 @@ from models.api.user import UserAPI
 # Then I should be able to log in with my new user
 def test_signup():
     # Given I am a new potential customer​
-    username = libs.utils.generate_string_with_prefix()
-    password = "test_1234?"
+    username = libs.utils.generate_string_with_prefix("user", 8)
+    password = "pass1234"
 
-    user_api = UserAPI('http://localhost:8000')
+    user_api = UserAPI(APP_BACKEND_URL)
 
     # When I signup in the app​
-    signup_api_response = user_api.signup(username,password)
+    signup_api_response = user_api.signup(username, password)
     assert signup_api_response.status_code == 200
 
     # Then I should be able to log in with my new user
-    login_api_response = user_api.login(username,password)
+    login_api_response = user_api.login(username, password)
     assert login_api_response.status_code == 200
 
 
@@ -27,5 +32,26 @@ def test_signup():
 # When I log in into the application​
 # Then I should see all my products
 def test_login():
-    # complete code
-    pass
+    username = USER_USERNAME
+    password = USER_PASSWORD
+    user_api = UserAPI(APP_BACKEND_URL)
+
+    # When I log in into the application​
+    login_api_response = user_api.login(username, password)
+    assert login_api_response.status_code == 200
+
+    token = login_api_response.json()["access_token"]
+    user_api.set_token(token)
+
+    # Then I should see all my products
+    user_data_api_response = user_api.get_user_data()
+    assert user_data_api_response.status_code == 200
+
+    products = user_data_api_response.json()["products"]
+
+    if products:
+        print(f"Found {len(products)} product(s): {[p['name'] for p in products]}")
+        assert len(products) > 0  # Verify at least 1 product
+    else:
+        print("No products assigned to this user.")
+        assert products == []  # Verify the list is empty
