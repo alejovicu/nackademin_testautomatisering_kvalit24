5a26456a69a861e0fe73c9d45966185ebb650b06
Mahta Ghorbani <mahta_gh@hotmail.com>
2025-10-03 12:58:21 +0200

    updated test user

--- labs/06_07_08_test_framework/test/integration/test_users.py
+++ labs/06_07_08_test_framework/test/integration/test_users.py
@@ -2,8 +2,7 @@ from playwright.sync_api import Page
 import pytest
 import libs.utils
 from models.api.user import UserAPI
-from models.ui.user import UserPage
-from models.ui.home import HomePage
+
 
 import os
 
@@ -12,53 +11,28 @@ FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost")
 
 
 
-@pytest.fixture
-def user_api():
-    return UserAPI(API_URL)
-
-
 # Given I am a new potential customer​
 # When I signup in the app​
 # Then I should be able to log in with my new user
-def test_signup_and_login(page: Page, user_api):
+def test_signup():
+    username = libs.utils.generate_string_with_prefix()
+    password = libs.utils.generate_string_with_prefix("pass_")
 
-    # Create a new user API
-    username = libs.utils.generate_string_with_prefix("user")
-    password = "test_1234?"
+    user_api = UserAPI(API_URL)
 
-    sigup_response = user_api.signup(username, password)
-    assert sigup_response.status_code == 200
+    signup_api_response = user_api.signup(username, password)
 
-    # Login with new user UI
-    page.goto(FRONTEND_URL)
-    page.locator("text=login").click()
-    page.locator("input[placeholder='Username']").fill (username)
-    page.locator("input[placeholder='Password']").fill(password)
-    page.locator("button:has-text('Login')").click()
+    assert signup_api_response.status_code == 200
+    
+    login_api_response = user_api.login(username, password)
 
-    page.wait_for_selector(f"text=Welcome, {username}!")
-    assert page.locator(f"text=Welcome, {username}!").is_visible()
+    assert login_api_response.status_code == 200
 
 
 # Given I am an authenticated user​
 # When I log in into the application​
 # Then I should see all my products
-def test_login_existing_user(page: Page):
-    username = "test"
-    password = "test123"
-
-    page.goto(FRONTEND_URL)
-    page.locator("text=login").click()
-    page.locator("input[placeholder='Username']").fill(username)
-    page.locator("input[placeholder='Password']").fill(password)
-    page.locator("button:has-text('Login')").click()
-
-    page.wait_for_selector(f"text=Welcome, {username}!")
-    assert page.locator(f"text=Welcome, {username}!").is_visible()
-
-    page.wait_for_selector("text=Your Products:")
-    product_elements = page.locator("text=Your Products: >> xpath=following-sibling::div/div")
-
-    assert product_elements.count() >= 0
-
-    
+def test_login():
+    user_api = UserAPI(API_URL)
+    login_response = user_api.login("test", "test123")
+    assert login_response.status_code == 200
