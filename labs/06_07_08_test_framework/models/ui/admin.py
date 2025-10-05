3618f2ace4b4e4f82b44bb64e2b6958d12cac7c3
arinagran <arinagran@gmail.com>
2025-10-05 21:20:04 +0200

    update lab_14

--- labs/06_07_08_test_framework/models/ui/admin.py
+++ labs/06_07_08_test_framework/models/ui/admin.py
@@ -7,12 +7,24 @@ class AdminPage:
         self.product_delete_buttons = page.get_by_role("button", name="Delete")
 
     def get_current_product_count(self):
+        try:
+            self.page.wait_for_selector(
+                "div.product-item", state="attached", timeout=3000
+            )
+        except:
+            pass
+        self.page.wait_for_load_state("networkidle")
         return self.product_items.count()
 
     def create_product(self, product_name):
         self.admin_input_product_name.fill(product_name)
         self.admin_btn_create.click()
+        self.page.wait_for_load_state("networkidle")
 
     def delete_product_by_name(self, product_name):
         row = self.page.locator(f"div.product-item:has-text('{product_name}')").first
         row.get_by_role("button", name="Delete").click()
+        try:
+            row.wait_for(state="detached", timeout=7000)
+        except:
+            pass
