2265881dd54614d5e0504d6523b3bcf0c482c8ec
Sabosm <sabrin.osman01@gmail.com>
2025-10-06 10:27:33 +0200

    changes

--- labs/06_07_08_test_framework/test/integration/test_users.py
+++ labs/06_07_08_test_framework/test/integration/test_users.py
@@ -9,7 +9,7 @@ def test_signup():
     username = libs.utils.generate_string_with_prefix()
     password = "1234"
 
-    user_api = UserAPI(VITE_BACKEND_URL)
+    user_api = UserAPI(BACKEND_URL)
 
     user_api.signup(username, password)
     assert user_api.status_code == 200
@@ -22,7 +22,7 @@ def test_login():
     username = libs.utils.generate_string_with_prefix()
     password = "1234"
 
-    user_api = UserAPI(VITE_BACKEND_URL)
+    user_api = UserAPI(BACKEND_URL)
     user_api.signup(username, password)
     assert user_api.status_code == 200
 
