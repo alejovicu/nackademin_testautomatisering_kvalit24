9940d3ad7b45ff3f3c89caeeb128bd042f55c1cd
Saga Andersson <sagander@hotmail.com>
2025-10-11 19:42:41 +0200

    done

--- labs/05_pom/models/home.py
+++ labs/05_pom/models/home.py
@@ -1,6 +1,26 @@
+from playwright.sync_api import Page, expect
+import re
+
 class HomePage:
-    def __init__(self, page):
+    def __init__(self, page: Page):
         self.page = page
+        self.base_url = "http://localhost:5173/"
 
     def navigate(self):
-        self.page.goto("http://localhost:5173/")
+        self.page.goto(self.base_url, wait_until="networkidle")
+
+    def add_product(self, product_name: str):
+        product = self.page.locator(".product-grid .product-item")
+        before = product.count()
+
+        self.page.get_by_role("textbox", name="Product Name").fill(product_name)
+        self.page.get_by_role("button", name=re.compile("Create Product", re.I)).click()
+
+        # Kolla att produkten finns
+        expect(self.page.locator(".product-grid .product-item").nth(-1)).to_contain_text(product_name)
+        expect(product).to_have_count(before + 1)
+
+    def delete_product(self, product_name: str):
+        self.page.locator(".product-item", has_text=product_name).get_by_role("button", name="Delete").click()
+        gone = self.page.locator(".product-grid .product-item").filter(has_text=product_name)
+        expect(gone).to_have_count(0)
--- labs/05_pom/tests/test_product.py
+++ labs/05_pom/tests/test_product.py
@@ -1,25 +1,48 @@
 from playwright.sync_api import Page, expect
-
-from models.home import HomePage
+from models.signup import SignupPage
 from models.login import LoginPage
-
+from models.home import HomePage
+import time
 
 def test_add_product_to_catalog(page: Page):
+    signup = SignupPage(page)
+    login = LoginPage(page)
+    home = HomePage(page)
 
-    #PO usage example
-    home_page = HomePage(page)
-    login_page = LoginPage(page)
-    home_page.navigate()
-    login_page.navigate_to_signup()
-
+    username = "admin"
+    password = "admin123"
+    product_name = f"product_{int(time.time())}"
 
     # Given I am an admin user​
+    signup.navigate()
+    signup.register_user(username, password)
+    login.login_user(username, password)
+
     # When I add a product to the catalog​
-    # Then The product is available to be used in the app
-    pass
+    home.add_product(product_name)
+
+    # Then The product is available to be used in the app​
+    expect(page.get_by_text(product_name)).to_be_visible()
 
 def test_remove_product_from_catalog(page: Page):
+    signup = SignupPage(page)
+    login = LoginPage(page)
+    home = HomePage(page)
+
+    username = "admin"
+    password = "admin123"
+    product_name = f"product_{int(time.time())}"
+
     # Given I am an admin user​
+    signup.navigate()
+    signup.register_user(username, password)
+    login.login_user(username, password)
+
+    # create product
+    home.add_product(product_name)
+
     # When I remove a product from the catalog​
-    # Then The product should not be listed in the app to be used
-    pass
+    home.delete_product(product_name)
+
+    # Then The product should not be listed in the app to be used​
+    expect(page.get_by_text(product_name)).not_to_be_visible()
