685111739ba39146ea7562d473ff466785b9e6fe
Gaffar inoussa <gaffarinoussa@hotmail.com>
2025-10-13 13:27:22 +0200

    test

--- labs/06_07_08_test_framework/config.py
+++ labs/06_07_08_test_framework/config.py
@@ -1,42 +1,66 @@
-import libs.utils
-
-from models.ui.home import HomePage
-from models.ui.signup import SignupPage
-from models.ui.user import UserPage
+import os
+from models.api.user import UserAPI
+from models.api.admin import AdminAPI
 
 
-class UsersFacade:
-    def __init__(self, page):
-        self.page = page
+def setup_admin():
 
-    def login_as_new_user(self):
-        # Complete code
-        home_page = HomePage(self.page)
-        signup_page = SignupPage(self.page)
-        # generate username
-        username = libs.utils.generate_string_with_prefix()
-        password = "password123"
-        # navigate to signup
-        home_page.navigate()
-        home_page.go_to_signup()
-        # create new user
-        signup_page.signup(username, password)
-        # navigate to login
-        signup_page.go_to_home()
-        # login as new user
-        home_page.login(username, password)
-        # return username and password
+    # setup admin user
+    admin_username = "test_admin"
+    admin_password = "test1234"
+    BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000/")
+    user_api = UserAPI(BACKEND_URL)
+    signup_response = user_api.signup(admin_username, admin_password)
 
-    def check_product_list(self):
-        # Given I am an authenticated user​
-        home_page = HomePage(self.page)
-        username = "test_user"
-        password = "test_password"
-        user_page = UserPage(self.page)
+    if signup_response.status_code == 200:
+        print("Admin user created successfully.")
+    else:
+        print("Admin user already exists or there was an error.")
 
-        # When I log in into the application​
-        home_page.navigate()
-        home_page.login(username, password)
-        # Then I should see all my products
-        products = user_page.get_user_products()
-        assert len(products) > 0
+    # login admin user
+    login_response = user_api.login(admin_username, admin_password)
+    assert login_response.status_code == 200, "Could not log in admin"
+    token = user_api.token
+
+    # create product
+    admin_api = AdminAPI(BACKEND_URL, token)
+    admin_api.create_product("TestProduct37")
+
+    # if create_product_response.status_code == 200:
+    #     print("Product created successfully.")
+    # else:
+    #     print("Product already exists or there was an error.")
+
+
+def setup_new_user():
+    BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000/")
+    user_api = UserAPI(BACKEND_URL)
+
+    # create new user
+    username = "test_user"
+    password = "test_password"
+    signup_response = user_api.signup(username, password)
+
+
+    if signup_response.status_code == 200:
+        print("New user created successfully.")
+    else:
+        print("User already exists or there was an error.")
+
+    # logga in newuser
+    login_response = user_api.login(username, password)
+    assert login_response.status_code == 200, "Could not log in new user"
+
+    # assign product to new user
+
+    add_product_response = user_api.add_product_to_user("TestProduct37")
+
+    if add_product_response.status_code == 200:
+        print("Product added to user successfully.")
+    else:
+        print("There was an error adding the product to the user.")
+
+
+if name == "main":
+    setup_admin()
+    setup_new_user()
